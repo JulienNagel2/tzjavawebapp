0c843a024e584b77b7eee274dba37d26fc8c4f91
Julien Nagel <jnagel@jnagel-a01.vmware.com>
2022-07-15 19:50:57 +0200

    this is new ver v2.9

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,9 +8,11 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		String monmot = "what a wonderful world of containers";
-		System.out.println("JNG msg for v2.8 : " + monmot);
-		return "Greetings from Spring Boot + Tanzu + JNG v2.8 : " + monmot;
+		String toReturnOriginal = "Greetings from Spring Boot + Tanzu!"; // use that to pass maven test!
+		String monmot = "what a wonderful world of containers indeed!";
+		System.out.println("JNG msg for v2.9 : " + monmot);
+		String toReturnModifiedByJNG = "Greetings from Spring Boot + Tanzu + JNG v2.9 : " + monmot;
+		return toReturnOriginal;
 	}
 
 }
